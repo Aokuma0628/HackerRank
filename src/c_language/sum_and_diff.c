039e3d065644cc07fd33466d638b5101a22e5592
TakeruNishimura <otyazuke1192@gmail.com>
2020-07-28 23:41:12 +0900

    modify task code of C

--- src/c_language/sum_and_diff.c
+++ src/c_language/sum_and_diff.c
@@ -1,89 +1,210 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define OK  1
+#define NG -1
+#define INPUT_NUM 2
+
+/**
+ * @struct READ_ALLOC
+ * @brief  list of string read from stdin
+ */
+typedef struct _read_alloc {
+  char *line;
+  struct _read_alloc *next;
+} READ_ALLOC;
+
+
+READ_ALLOC *g_read_list = NULL;   //! List pointer to read data
+READ_ALLOC *g_read_last  = NULL;  //! end of list
+
+
+/*read from stdin */
 char *readline();
-void  separateblank(char *target, char **str1, char **str2);
+int   add_read_alloc(char *line);
+void  free_read_list();
 
-int main(
+
+/**
+* @brief      read string from stdin
+* @param[in]  void
+* @return     char* : one-line string
+*/
+char *readline(
   void
 )
 {
-  char *int_str = NULL;
-  char *flt_str = NULL;
+  size_t alloc_length = 1024;
+  size_t data_length  = 0;
+  char  *data         = (char *)malloc(alloc_length);
 
-  int_str = readline();
-  if (int_str) {
-    char *str_p1 = NULL;
-    char *str_p2 = NULL;
+  while (1) {
+    char *cursor = data + data_length;
+    char *line   = fgets(cursor, alloc_length - data_length, stdin);
+    if (!line) {
+      break;
+    }
 
-    separateblank(int_str, &str_p1, &str_p2);
+    data_length += strlen(cursor);
+
+    if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+      break;
+    }
+
+    size_t new_length = alloc_length << 1;
+    data = (char *)realloc(data, new_length);
+
+    if (!data) {
+      break;
+    }
+
+    alloc_length = new_length;
   }
 
-  flt_str = readline();
+  if (data[data_length - 1] == '\n') {
+    data[data_length - 1] = '\0';
+  }
 
+  data = (char *)realloc(data, data_length);
+  if (add_read_alloc(data) != OK) {
+    return NULL;
+  }
 
-
-  return 0;
+  return data;
 }
 
-void  separateblank(
-  char *target,
-  char **str_p1,
-  char **str_p2
+
+/**
+* @brief      add read list
+* @param[in]  line : string from stdin
+* @return     int : add result
+*/
+int add_read_alloc (
+  char *line
 )
 {
-while(*target != '\0') {
-  
+  int ret = NG;
+  READ_ALLOC *alloc = NULL;
+  READ_ALLOC *last  = g_read_last;
+
+  alloc = (READ_ALLOC*)calloc(1, sizeof(READ_ALLOC));
+  if (!alloc) {
+    return ret;
+  }
+  alloc->line = line;
+
+  if (!g_read_list) {
+    g_read_list = alloc;
+    g_read_last  = alloc;
+  }
+  else {
+    last->next = alloc;
+    g_read_last = last->next;
+  }
+
+  ret = OK;
+  return ret;
 }
+
+
+/**
+* @brief      free read list
+* @param[in]  void
+* @return     void
+*/
+void free_read_list() {
+  READ_ALLOC *list = g_read_list;
+  READ_ALLOC *tmp  = NULL;
+
+  while(list) {
+    tmp = list->next;
+    free(list->line);
+    free(list);
+    list = tmp;
+  }
 
   return ;
 }
 
 
-char *readline(
-  void
+int split_integer(
+  char *str,
+  int   val[INPUT_NUM]
 )
 {
-  size_t alloc_length = 256;
-  size_t data_length  = 0;
-  char*  data         = (char *)malloc(alloc_length);
+  int   tmp_val = 0;
+  int   i       = 0;
+  char *tmp_str = str;
+  char *err     = NULL;
 
-  if (!data) {
-    return NULL;
+  if (!str || !val) {
+    return NG;
   }
 
-  while (1) {
-      char* cursor = data + data_length;
-      char* line   = fgets(cursor, alloc_length - data_length, stdin);
-      /*malloc null check*/
-      if (!line) {
-        break;
-      }
+  for (; i < INPUT_NUM - 1; i++) {
+    char *space = NULL;
 
-      data_length += strlen(cursor);
-      /*check if read one sentence*/
-      if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
-        break;
-      }
-
-      /*when one sentence length is longer than alloc_length */
-      size_t new_length = alloc_length << 1;
-
-      data = (char *)realloc(data, new_length);
-      if (!data) {
-        break;
-      }
-
-      alloc_length = new_length;
+    tmp_val = strtol(tmp_str, &space, 10);
+    if (*space != ' ') {
+      return NG;
+    }
+    val[i] = tmp_val;
+    tmp_str++;
   }
 
-  if (data[data_length - 1] == '\n') {
-      data[data_length - 1] = '\0';
+  tmp_val = strtol(tmp_str, &err, 10);
+  if (*err != '\0') {
+    return NG;
+  }
+  val[INPUT_NUM] = tmp_val;
+
+  return OK;
+}
+
+int split_float(
+  char *str,
+  float val[INPUT_NUM]
+)
+{
+  int ret = NG;
+
+  if (!str || !val) {
+
   }
 
-  /*Making the just data size*/
-  data = (char *)realloc(data, data_length);
 
-  return data;
+  return ret;
+}
+
+
+/**
+* @brief      main function
+* @param[in]  void
+* @return     int : 0
+*/
+int main()
+{
+  int   val_int[INPUT_NUM];
+  float val_flt[INPUT_NUM];
+  char *str_int = readline();
+  char *str_flt = readline();
+
+   if (split_integer(str_int, val_int) != OK) {
+     goto l_end;
+   }
+   if (split_float(str_flt, val_flt) != OK) {
+     goto l_end;
+   }
+
+   printf("%d %d\n",
+          val_int[0] + val_int[1],
+          val_int[0] - val_int[1]);
+   printf("%.1f %.1f\n",
+          val_flt[0] + val_flt[1],
+          val_flt[0] - val_flt[1]);
+
+l_end:
+  free_read_list();
+  return 0;
 }
