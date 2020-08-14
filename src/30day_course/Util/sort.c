1c05c9237f1ecdaf7d684eccee2b9f5abdc3ac5a
TakeruNishimura <otyazuke1192@gmail.com>
2020-08-14 23:41:14 +0900

    add util function

--- src/30day_course/Util/sort.c
+++ src/30day_course/Util/sort.c
@@ -35,3 +35,34 @@ int bubble_sort(
 
   return OK;
 }
+
+/**
+* @brief      bubble_sort_str
+* @param[in]  array : sort target
+* @param[in]  num   : array length
+* @return     int   : sort result
+*/
+int bubble_sort_str(
+  char **array,
+  int  num
+)
+{
+  int i   = 0;
+
+  if (!array) {
+    return NG;
+  }
+
+  for (; i < num; i++)  {
+    int j = 0;
+    for (; j < (num - i - 1); j++) {
+      if (strcmp(array[j], array[j + 1]) > 0) {
+          char *tmp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = tmp;
+      }
+    }
+  }
+
+  return OK;
+}
--- src/30day_course/Util/string.c
+++ src/30day_course/Util/string.c
@@ -89,3 +89,34 @@ int split_str2int(
 
   return OK;
 }
+
+
+/**
+* @brief      Regular expression string checking
+* @param[in]  pattern : Regular expression search string
+* @param[in]  target  : Search target string
+* @return     int     : match result
+*/
+int check_string_match(
+  const char  *pattern,
+  char        *target
+)
+{
+  regex_t preg;
+  int ret = NG;
+
+  if (regcomp(&preg, pattern,
+        REG_EXTENDED | REG_NOSUB | REG_NEWLINE) != 0) {
+    return ret;
+  }
+
+  if (regexec(&preg, target, 0, NULL, 0) != 0) {
+    ret = NG;
+  }
+  else {
+    ret = OK;
+  }
+  regfree(&preg);
+
+  return ret;
+}
