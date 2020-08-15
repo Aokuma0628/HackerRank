ea3b7768685d3f5735d9e34efe814c8aba1dbe11
TakeruNishimura <otyazuke1192@gmail.com>
2020-08-15 23:06:35 +0900

    add C_function code complete!

---
+++ src/c_language/function.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(
+  int a,
+  int b,
+  int c,
+  int d
+)
+{
+  if (a > b) {
+    if (a > c) {
+      if (a > d) {
+        return a;
+      }
+    }
+  }
+  if (b > c) {
+    if (b > d) {
+      return b;
+    }
+  }
+  if (c > d) {
+    return c;
+  }
+  return d;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
